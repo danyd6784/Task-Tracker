df8065528b9ae91a1c64b80ec80a6dcf3099fe10
Dany Delarosa <104733422+danyd6784@users.noreply.github.com>
2026-04-13 22:07:44 -0400

    Feat: Add input validation prior to processing and error handling

--- task-cli.js
+++ task-cli.js
@@ -11,4 +11,22 @@ class Task{
     }
 }
 
+const allowedUtilityCommands = ["ADD", "UPDATE", "DELETE", "MARK-IN-PROGRESS", "MARK-DONE", "LIST"]
+const args = process.argv.slice(2);
 
+try{
+    if (typeof args[0] === "string"){
+        //normalize input and compare against array of allowed values
+        let normalizedInput = args[0].toUpperCase()
+        if (allowedUtilityCommands.indexOf(normalizedInput) !== -1) {
+            const command = normalizedInput
+            console.log(command);
+        } else {
+            throw new Error("Error: command parameter must be an allowed value of { Add | Update | Delete | Mark-in-progress | Mark-done | List }")
+        }
+    } else {
+        throw new Error("Error: Parameter must be a string in { Add | Update | Delete | Mark-in-progress | Mark-done | List }")
+    }
+} catch(err){
+    console.warn(err.message);
+}
